64c4365f4d2fc2552ad2d2b51f03a072d61e4964
kisenhuang <huangweiyao@tal.com>
2021-11-23 18:59:36 +0800

    完善代码

--- tokio_client/src/main.rs
+++ tokio_client/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpStream;
+use std::error::Error;
+use std::io::Write;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+    stream.write_all(b"hello").await?;
+
+    let mut buf = [0; 1024];
+    let n = stream.read(&mut buf).await?;
+
+    println!("GET {}", String::from_utf8_lossy(&buf[0..n]));
+
+    Ok(())
 }
--- tokio_server/src/main.rs
+++ tokio_server/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    println!("启动服务...");
+
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+
+        println!("请求开始，访问地址 {}", addr);
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                println!("接收数据 {:?}", String::from_utf8_lossy(&buf[0..n]));
+
+                // Write the data back
+                if let Err(e) = socket.write_all("嗯哼".as_bytes()).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+
+            }
+        });
+    }
 }
